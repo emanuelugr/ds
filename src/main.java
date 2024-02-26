5a3b20c4f311b9427402c4a37cdae1a10b6736cc
TimurSorokin <timursorokin94@gmail.com>
2024-02-26 17:12:57 +0100

    diagrama

--- src/main.java
+++ src/main.java
@@ -5,6 +5,14 @@
  * FactoriaCarreraYBicicleta: Timur
 */
 
+
+public interface FactoriaCarreraYBicicleta{
+    public Carrera crearCarrera();
+    public Bicicleta crearBicicleta();
+
+}
+
+
 class Main{
     public static void main(String[] args){
 
