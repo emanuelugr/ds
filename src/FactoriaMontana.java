7c3b0fdff77a683631f887a8d892ffcee1394bc5
TimurSorokin <timursorokin94@gmail.com>
2024-02-26 18:30:05 +0100

    Factoria implementadas

--- src/FactoriaMontana.java
+++ src/FactoriaMontana.java
@@ -2,8 +2,15 @@
  * FactoriaMontana
  */
 public class FactoriaMontana implements FactoriaCarreraYBicicleta{
+    
+    @Override
     public Carrera crearCarrera(){
         return new CarreraMontana();
+    }
+
+    @Override
+    public Bicicleta crearBicicleta() {
+        return new BicicletaMontana();
     }
     
 }
