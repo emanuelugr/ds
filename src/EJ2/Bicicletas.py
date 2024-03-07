bb42f1ab43f3e5ad93952cd166f3198aecaa3e1a
TimurSorokin <timursorokin94@gmail.com>
2024-03-07 10:52:17 +0100

    Ejercicio 2 Prototipo Terminado

--- src/EJ2/Bicicletas.py
+++ src/EJ2/Bicicletas.py
@@ -1,9 +1,10 @@
 from abc import ABC, abstractclassmethod
+import copy
 
 class Bicicleta(ABC):
 	def __init__(self,id: int):
 		self.id = id
-
+	
 
 class BicicletaCarretera(Bicicleta):
 	def __init__(self, id: int):
@@ -13,3 +14,20 @@ class BicicletaCarretera(Bicicleta):
 class BicicletaMontana(Bicicleta):
 	def __init__(self, id: int):
 		super().__init__(id)
+
+
+
+class BicicletaPrototipo:
+	def __init__(self):
+		self.bicicletas = dict()
+
+	def register_bicicleta(self,name:str,bicicleta:Bicicleta)->None:
+		self.bicicletas[name] = bicicleta
+
+	def unregister_bicicleta(self,name:str)->None:
+		del self.bicicletas[name]
+
+	def clone(self,name:str, **attrs:dict)->Bicicleta:
+		bicicleta = copy.deepcopy(self.bicicletas.get(name))
+		bicicleta.__dict__.update(attrs)
+		return bicicleta
--- src/EJ2/main.py
+++ src/EJ2/main.py
@@ -1,26 +1,46 @@
 import Factorias
-
+import Carreras
+import Bicicletas
 
 #Carretera
 def TestCarretera(num:int)->None:
+	print("===========TEST FACTORIA CARRETERA===========")
 	factoria = Factorias.FactoriaCarretera()
 	carrera = factoria.crearCarrera()
 	for i in range(0,num):
 		bicicleta = factoria.crearBicicleta(i)
 		carrera.AddBicicleta(bicicleta)
 	carrera.Run()
+	print("===========FIN===========")
 
 #Montaña
 def TestMontana(num:int)->None:
+	print("===========TEST FACTORIA MONTAÑA===========")
 	factoria = Factorias.FactoriaMontana()
 	carrera = factoria.crearCarrera()
 	for i in range(0,num):
 		bicicleta = factoria.crearBicicleta(i)
 		carrera.AddBicicleta(bicicleta)
 	carrera.Run()
+	print("===========FIN===========")
 	
+#Prototipo
+def TestPrototipo(num:int)->None:
+	print("===========TEST PROTOTIPO===========")
+	carrera = Carreras.CarreraMontana()
+	prototipo = Bicicletas.BicicletaPrototipo()
+
+	prototipo.register_bicicleta("mountain",Bicicletas.BicicletaMontana(0))
+	for i in range(0,num):
+		bicicleta = prototipo.clone("mountain", id=i)
+		carrera.AddBicicleta(bicicleta)
+	carrera.Run()
+	print("===========FIN===========")
+
+
 
 if __name__=="__main__":
 	num = 10
 	TestCarretera(num)
 	TestMontana(num)
+	TestPrototipo(num)
