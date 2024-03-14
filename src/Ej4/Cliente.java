615224cf7932019a902a7fd9d5f8aa0fc3d4f04b
Alejandroibo <alejandroibo131313@gmail.com>
2024-03-14 18:56:23 +0100

    Añadiendo codigo de Thomas

--- src/Ej4/Cliente.java
+++ src/Ej4/Cliente.java
@@ -1,14 +1,136 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JToggleButton;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Cliente {
 
 	private GestorFiltros gestorFiltros ;
+    private JLabel estadoMotorLabel;
+    private JToggleButton botonEncender, botonAcelerar, botonFrenar;	
+	private double velocidad = 0;
 
-		public Cliente ( GestorFiltros gestorFiltros ) {
-		this.gestorFiltros = gestorFiltros ;
+	public Cliente(GestorFiltros gestorFiltros) {
+
+		this.gestorFiltros = gestorFiltros;
+
+		JFrame ventana = new JFrame("Simulador de Motor");
+		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		ventana.setSize(400, 150);
+		ventana.setVisible(true);
+
+		botonEncender=new JToggleButton("ENCENDER");
+		botonEncender.setForeground(Color.green);
+		botonAcelerar=new JToggleButton("ACELERAR");
+		botonFrenar= new JToggleButton("FRENAR");
+
+
+		// Agregar los botones al panel
+		JPanel panelBotones = new JPanel();
+		panelBotones.add(botonEncender);
+		panelBotones.add(botonAcelerar);
+		panelBotones.add(botonFrenar);
+
+		ActionListener BE = new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (botonEncender.isSelected()) {
+					estadoMotorLabel.setText("ENCENDIDO");
+					botonEncender.setText("APAGAR");
+					botonEncender.setForeground(Color.red);
+					botonAcelerar.setEnabled(true);
+					botonFrenar.setEnabled(true);
+					gestorFiltros.ejecutar(velocidad, EstadoMotor.ENCENDIDO);
+				} else {
+					estadoMotorLabel.setText("APAGADO");
+					botonEncender.setText("ENCENDER");
+					botonEncender.setForeground(Color.green);
+					botonAcelerar.setEnabled(false);
+					botonFrenar.setEnabled(false);
+				}
+			}
+		};
+
+		botonEncender.addActionListener(new ActionListener()) {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (botonEncender.isSelected()) {
+					estadoMotorLabel.setText("ENCENDIDO");
+					botonEncender.setText("APAGAR");
+					botonEncender.setForeground(Color.red);
+					botonAcelerar.setEnabled(true);
+					botonFrenar.setEnabled(true);
+					gestorFiltros.toggleEngineState(EstadoMotor.FRENANDO);
+					gestorFiltros.start();
+				} else {
+					estadoMotorLabel.setText("APAGADO");
+					botonEncender.setText("ENCENDER");
+					botonEncender.setForeground(Color.green);
+					botonAcelerar.setEnabled(false);
+					botonFrenar.setEnabled(false);
+					gestorFiltros.toggleEngineState(EstadoMotor.APAGADO);
+				}
+			}});
+
+		botonAcelerar.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (botonEncender.isSelected() && botonAcelerar.isSelected()) {
+					estadoMotorLabel.setText("ACELERANDO");
+					botonAcelerar.setText("Soltar Accelerar");
+					botonAcelerar.setForeground(Color.red);
+					botonFrenar.setSelected(false);
+					botonFrenar.setText("Frenar");
+					gestorFiltros.toggleEngineState(EstadoMotor.ACELERANDO);
+				botonFrenar.setForeground(Color.black);
+				
+				}else if(botonEncender.isSelected() && !botonAcelerar.isSelected()){
+				estadoMotorLabel.setText("FRENANDO");
+				botonAcelerar.setText("Accelerar");
+				botonAcelerar.setForeground(Color.black);
+				
+
+				}
+			}
+		});
+
+
+		botonFrenar.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (botonEncender.isSelected() && botonFrenar.isSelected()) {
+					estadoMotorLabel.setText("FRENANDO");
+					botonFrenar.setText("Soltar Frenar");
+					botonFrenar.setForeground(Color.red);
+					botonAcelerar.setSelected(false);
+					botonAcelerar.setText("Accelerar");
+					botonAcelerar.setForeground(Color.black);
+					gestorFiltros.toggleEngineState(EstadoMotor.FRENANDO);
+					
+				}else if(botonEncender.isSelected() && !botonFrenar.isSelected()){
+				botonFrenar.setText("Frenar");
+				botonFrenar.setForeground(Color.black);
+				
+				}
+			}
+		});
+
+
+		JPanel panel1 = new JPanel();
+		estadoMotorLabel = new JLabel("APAGADO", JLabel.CENTER);
+		estadoMotorLabel.setFont(new Font("Arial", Font.BOLD, 24));
+		estadoMotorLabel.setForeground(Color.red);
+
+		panel1.add(estadoMotorLabel, BorderLayout.NORTH);
+
+
+		ventana.add(panel1);
+		ventana.add(panelBotones);
+		ventana.setLayout(new FlowLayout());
+		ventana.setVisible(true);
 	}
-/* 
-	public void ejecutar ( String content ) {
-		Message message = new Message ( content ) ;
-		filterManager . postMessage ( message ) ;
-	}
-*/
+
 }
--- src/Ej4/Objetivo.java
+++ src/Ej4/Objetivo.java
@@ -1,4 +1,11 @@
-public class Objetivo {
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import java.awt.*;
+
+public class Objetivo extends JPanel{
 	/* Puede que quede mejor que esta variable se transmita desde el cliente */
 	private final double RADIO = 0.15;
 
@@ -9,6 +16,52 @@ public class Objetivo {
 	 * ejecutar se debe encargar de hacer todas las operaciones de modificar los atributos segun las revoluciones
 	 * 
 	 */
+
+	
+	//Codigo de thomas // --------------------------------------------
+  	public Objetivo(){
+		this.velocimetro = 0;
+		this.RPM = 0;
+		//kilometros=0;
+
+		JFrame frame =new JFrame("Salpicadero");
+		frame.setTitle("Salpicadero");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(400, 200);
+
+		JPanel mainPanel = new JPanel();
+		mainPanel.setLayout(new GridLayout(3, 1));
+
+		JPanel p1=new JPanel();
+		JLabel l11=new JLabel("km/h:");
+		JTextField l12 = new JTextField(10);
+		l12.setEditable(false);
+		p1.add(l11);
+		p1.add(l12);
+		
+		JPanel p2=new JPanel();
+		JLabel l21=new JLabel("contador reciente");
+		JTextField l22 = new JTextField(10);
+		l22.setEditable(false);
+		p2.add(l21);
+		p2.add(l22);
+
+		JPanel p3=new JPanel();
+		JLabel l31=new JLabel("RPM");
+		JTextField l32 = new JTextField(10);
+		l32.setEditable(false);
+		p3.add(l31);
+		p3.add(l32);
+
+		mainPanel.add(p1);
+		mainPanel.add(p2);
+		mainPanel.add(p3);
+		
+		frame.add(mainPanel);
+		frame.setLayout(new FlowLayout());
+		frame.setVisible(true);
+  	}	 
+	//FIn COdigo de thomas // --------------------------------------------
 
 	public double ejecutar(double revoluciones, EstadoMotor estadoMotor){
 
