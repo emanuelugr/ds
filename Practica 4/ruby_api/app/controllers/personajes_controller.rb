58fb23f0b7bfce40f2205bba158274c6d03c055b
Timur Sorokin <timursorokin94@gmail.com>
2024-05-21 20:38:34 +0200

    ruby_api visible just in case

---
+++ Practica 4/ruby_api/app/controllers/personajes_controller.rb
@@ -0,0 +1,44 @@
+class PersonajesController < ApplicationController
+
+    def index
+        @personajes = Personaje.where(usuario: params[:usuario])
+        #@personajes = Personaje.all
+        logger.debug "Usuario param: #{params[:usuario]}"
+        logger.info "Cargando los personajes del usuario: #{params[:usuario]}, Total: #{@personajes.count}"
+        render json: @personajes
+    end
+
+    def create
+        @personaje = Personaje.new(personaje_params)
+        if @personaje.save
+            render json: @personaje, status: :created
+        else
+            render json: @personaje.errors, status: :unprocessable_entity
+        end
+    end
+
+
+    def update
+        @personaje = Personaje.find(params[:id])
+        if @personaje.update(personaje_params)
+            render json: @personaje
+        else
+            render json: @personaje.errors, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        personaje = Personaje.find(params[:id])
+        if personaje.destroy
+            head :ok
+        else
+            render json: { error: "Failed to delete" }, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def personaje_params
+        params.require(:personaje).permit(:usuario,:nombre, :raza, :clase, primAttr: {}, secAttr: {})
+    end
+end
---
+++ Practica 4/ruby_api/db/migrate/20240521182716_create_personajes.rb
@@ -0,0 +1,14 @@
+class CreatePersonajes < ActiveRecord::Migration[7.1]
+  def change
+    create_table :personajes do |t|
+      t.string :nombre
+      t.string :raza
+      t.string :clase
+      t.json :primAttr
+      t.json :secAttr
+      t.string :usuario
+
+      t.timestamps
+    end
+  end
+end
